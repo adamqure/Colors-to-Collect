74b45158bdd0ccd44577c28431c0cc3941a38154
Adam Ure <adamqure@icloud.com>
2021-03-27 11:18:33 -0600

    Initial Commit

---
+++ Colors to CollectTests/Colors_to_CollectTests.swift
@@ -0,0 +1,33 @@
+//
+//  Colors_to_CollectTests.swift
+//  Colors to CollectTests
+//
+//  Created by Adam Ure on 3/27/21.
+//
+
+import XCTest
+@testable import Colors_to_Collect
+
+class Colors_to_CollectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
